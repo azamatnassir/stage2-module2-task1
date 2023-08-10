15cf0bf40d90d10f89370e77d683ee297e424840
azamatnassir <azamatnsr@gmail.com>
2023-08-10 16:42:52 +0600

    commit

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,27 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        String firstName = req.getParameter("firstName");
+        String secondName = req.getParameter("secondName");
+
+        User user = new User(firstName, secondName);
+        Warehouse.getInstance().addUser(user);
+
+        req.setAttribute("user", user);
+        resp.sendRedirect("/add");
+    }
 }
